9fb5d1c88c01cc9c7bc8940789bec5d4f468e03e
Aaron Li <aaronlee916@qq.com>
2024-09-05 16:31:20 +0800

    完善部分接口

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-import express from 'express'
-import fs from 'fs'
-const app = express()
+import express from "express";
+import fs from "fs";
+import { UserData } from "./assets/data/userdata.js";
+
+const app = express();
 
 //获取用户的头像
-app.get('/v1/getAvatar',(req,res)=>{
-    const dirname='./assets/images/avatars/'
-    let id=req.query.id
-    fs.readFile(dirname+`${id}.png`,(err,data)=>{
-        if(!err){
-            res.send(data)
-        }
-        else{
-            console.log(err)
-        }
-    })
-})
+app.get("/v1/getAvatar", (req, res) => {
+  const dirname = "./assets/images/avatars/";
+  let id = req.query.id;
+  fs.readFile(dirname + `${id}.png`, (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      console.log(err);
+    }
+  });
+});
+//获取单个用户信息
+app.get("/v1/getUser", (req, res) => {
+  let id = req.query.id;
+  for (let item of UserData) {
+    if (id == item.id) {
+      res.send(item);
+      break;
+    }
+  }
+});
+//添加用户
+app.post("/v1/addUser", (req, res) => {
 
+});
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000')
-})
+  console.log("Server running on http://localhost:3000");
+});
