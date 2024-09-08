c4926f68dc87a19fab3edd6bc13c612fb1954634
Aaron Li <aaronlee916@qq.com>
2024-09-08 17:22:54 +0800

    updateUser完成

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import fs from "fs";
-import { UserData } from "./assets/data/userdata.js";
+import UserData from "./assets/data/userdata.js";
 import { trendingCV, trendingStaff } from "./assets/data/trending.js";
 
-
 const app = express();
+app.use(express.json()); // 用于解析 JSON 类型的请求体
+app.use(express.urlencoded({ extended: true })); // 用于解析 URL-encoded 类型的请求体
 
 const avatarRoot = "./assets/images/avatars/";
-const dataRoot= "./assets/data/"
+const dataRoot = "./assets/data/";
 
 //获取用户的头像
 app.get("/v1/getAvatar", (req, res) => {
-  
   let id = req.query.id;
   fs.readFile(avatarRoot + `${id}.png`, (err, data) => {
     if (!err) {
@@ -22,77 +22,84 @@ app.get("/v1/getAvatar", (req, res) => {
   });
 });
 //获取人气最高CV
-app.get('/v1/getTrendingCV',(req,res)=>{
-  res.send(trendingCV)
-})
+app.get("/v1/getTrendingCV", (req, res) => {
+  res.send(trendingCV);
+});
 //获取人气最高Staff
-app.get('/v1/getTrendingStaff',(req,res)=>{
-  res.send(trendingStaff)
-})
+app.get("/v1/getTrendingStaff", (req, res) => {
+  res.send(trendingStaff);
+});
 //获取所有用户
-app.get('/v1/getAllUsers',(req,res)=>{
-    res.send(UserData)
-})
+app.get("/v1/getAllUsers", (req, res) => {
+  res.send(UserData);
+});
 //获取单个用户信息
 app.get("/v1/getUser", (req, res) => {
   let id = req.query.id;
   for (let item of UserData) {
     if (id == item.id) {
-      res.send(item)
+      res.send(item);
       break;
     }
   }
 });
 //添加用户,会写入文件
 app.post("/v1/addUser", (req, res) => {
-  UserData.push(req.query.user)
-  fs.writeFile(dataRoot+'userdata.js',UserData,(err)=>{
-    if(!err){
-      res.send('Success!')
+  UserData.push(req.query);
+  fs.writeFile(
+    dataRoot + "userdata.js",
+    "export default " + JSON.stringify(UserData),
+    (err) => {
+      if (!err) {
+        res.send("Success!");
+      } else {
+        res.send(err);
+      }
     }
-    else{
-      res.send(err)
-    }
-  })
-  res.send(UserData)
+  );
+  res.send(UserData);
 });
 app.post("/v1/updateUser", (req, res) => {
-  let newUserData=[]
-  UserData.map((item)=>{
-    if(item.id==req.query.user.id){
-      newUserData.push(req.query.user)
+  let newUserData = [];
+  UserData.map((item) => {
+    if (item.id == req.body.id) {
+      newUserData.push(req.body);
+    } else {
+      newUserData.push(item);
     }
-    else{
-      newUserData.push(item)
+  });
+  fs.writeFile(
+    dataRoot + "userdata.js",
+    "export default " + JSON.stringify(newUserData),
+    (err) => {
+      if (!err) {
+        res.send(newUserData);
+      } else {
+        res.send(err);
+      }
     }
-  })
-  fs.writeFile(dataRoot+'userdata.js',newUserData,(err)=>{
-    if(!err){
-      res.send(newUserData)
-      res.send('Success!')
-    }
-    else{
-      res.send(err)
-    }
-  })
+  );
 });
 //删除用户信息，会写入文件
-app.delete('/v1/deleteUser',(req,res)=>{
+app.delete("/v1/deleteUser", (req, res) => {
   for (let item of UserData) {
     if (req.query.id == item.id) {
-      UserData.splice(req.query.id,1)
+      UserData.splice(req.query.id, 1);
       break;
     }
   }
-  fs.writeFile(dataRoot+'userdata.js',UserData,(err)=>{
-    if(!err){
-      res.send('Success!')
+  fs.writeFile(
+    dataRoot + "userdata.js",
+    "export default " + JSON.stringify(UserData),
+    (err) => {
+      if (!err) {
+        res.send("Success!");
+      } else {
+        res.send(err);
+      }
     }
-    else{
-      res.send(err)
-    }
-  })
-})
-app.listen( process.env.PORT||3000, () => {
+  );
+});
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on https://voiceradarserver.onrender.com`);
 });
