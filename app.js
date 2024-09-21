24538ce085533be2cd204b4f1153e5f9520a944a
Aaron Li <aaronlee916@qq.com>
2024-09-21 21:20:07 +0800

    加入jwt

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import { promisify } from "util";
 import multer from "multer";
 import { PrismaClient } from "@prisma/client";
 import { encryptPassword } from "./utils/md5.js";
+import { generateToken, validateToken } from "./utils/jwt.js";
 
 const app = express();
 app.use(express.json()); // 用于解析 JSON 类型的请求体
@@ -19,179 +20,164 @@ const writeFile = promisify(fs.writeFile);
 const rename = promisify(fs.rename);
 const uploads = multer({ dest: "./public" });
 const prisma = new PrismaClient();
+const router = express.Router();
 
-//获取用户的头像
-app.get("/v1/getAvatar", (req, res) => {
-  let id = req.query.id;
-  readFile(avatarRoot + `${id}.png`, (err, data) => {
-    if (!err) {
-      res.set("Content-Type", "image/jpeg");
-      res.send(data);
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-//获取人气最高CV
-app.get("/v1/getTrendingCV", (req, res) => {
-  res.send(trendingCV);
-});
-//获取人气最高Staff
-app.get("/v1/getTrendingStaff", (req, res) => {
-  res.send(trendingStaff);
-});
-//获取所有用户
-app.get("/v1/getAllUsers", async (req, res) => {
-  let allUsers = await prisma.user.findMany();
-  res.send(allUsers);
-});
-//获取单个用户信息
-app.get("/v1/getUser", async (req, res) => {
-  let id = Number.parseInt(req.query.id);
-  let user = await prisma.user.findUnique({
-    where: {
-      id,
-    },
-  });
-  res.send(user);
-});
-//添加用户,会写入文件
-app.post("/v1/addUser", async (req, res) => {
-  await prisma.user.create({
-    data: {
-      name: req.body.name,
-      sex: req.body.sex,
-      password: encryptPassword(req.body.password),
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      UserDescription: req.body.UserDescription,
-      avatarLink: req.body.avatarLink,
-    },
-  });
-  res.send(
-    `用户${req.body.name}创建成功,ID${await prisma.user.findUnique({
+router
+  .get("/v1/getAvatar", validateToken, (req, res) => {
+    let id = req.query.id;
+    readFile(avatarRoot + `${id}.png`, (err, data) => {
+      if (!err) {
+        res.set("Content-Type", "image/jpeg");
+        res.send(data);
+      } else {
+        console.log(err);
+      }
+    });
+  })
+  .get("/v1/getTrendingCV", validateToken, (req, res) => {
+    res.send(trendingCV);
+  })
+  .get("/v1/getTrendingStaff", validateToken, (req, res) => {
+    res.send(trendingStaff);
+  })
+  .get("/v1/getAllUsers", validateToken, async (req, res) => {
+    let allUsers = await prisma.user.findMany();
+    res.send(allUsers);
+  })
+  .get("/v1/getUser", validateToken, async (req, res) => {
+    let id = Number.parseInt(req.query.id);
+    let user = await prisma.user.findUnique({
       where: {
-        email: req.body.email,
+        id,
       },
-    })}`
-  );
-});
-
-app.post("/v1/addArtist", async (req, res) => {
-  await prisma.artist.create({
-    name: req.body.name,
-    weiboLink: req.body.weiboLink,
-    qq: req.body.qq,
-    isCV: req.body.isCV,
-    isStaff: req.body.isStaff,
-    voiceType: req.body.voiceType,
-    soundPressure: req.body.soundPressure,
-    demoLink: req.body.demoLink,
-    artistDescription: req.body.artistDescription,
-  });
-});
-
-app.post("/v1/updateUser", async (req, res) => {
-  let updatedUser = req.body;
-  let id = req.query.id;
-  await prisma.user.update({
-    where: {
-      id,
-    },
-    data: {
-      name: updatedUser.name,
-      sex: updatedUser.sex,
-      password: encryptPassword(updatedUser.password),
-      phoneNumber: updatedUser.phoneNumber,
-      UserDescription: updatedUser.UserDescription,
-      email: updatedUser.email,
-    },
-  });
-  console.log(await prisma.user.findMany());
-});
-
-app.post("/v1/updateArtist", async (req, res) => {
-  await prisma.artist.update({
-    name: req.body.name,
-    weiboLink: req.body.weiboLink,
-    qq: req.body.qq,
-    isCV: req.body.isCV,
-    isStaff: req.body.isStaff,
-    voiceType: req.body.voiceType,
-    soundPressure: req.body.soundPressure,
-    demoLink: req.body.demoLink,
-    artistDescription: req.body.artistDescription,
-  });
-});
-
-//搜索CV API，目前只能GET用户名完全一致的用户
-app.get("/v1/searchCV", async (req, res) => {
-  res.send(
-    await prisma.artist.findMany({
+    });
+    res.send(user);
+  })
+  .post("/v1/addUser", async (req, res) => {
+    let newUser = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        sex: req.body.sex,
+        password: encryptPassword(req.body.password),
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        UserDescription: req.body.UserDescription,
+        avatarLink: req.body.avatarLink,
+      },
+    });
+    newUser.token = await generateToken(newUser);
+    res.send(newUser);
+  })
+  .post("/v1/addArtist", validateToken, async (req, res) => {
+    await prisma.artist.create({
+      name: req.body.name,
+      weiboLink: req.body.weiboLink,
+      qq: req.body.qq,
+      isCV: req.body.isCV,
+      isStaff: req.body.isStaff,
+      voiceType: req.body.voiceType,
+      soundPressure: req.body.soundPressure,
+      demoLink: req.body.demoLink,
+      artistDescription: req.body.artistDescription,
+    });
+  })
+  .post("/v1/updateUser", validateToken, async (req, res) => {
+    let updatedUser = req.body;
+    let id = req.query.id;
+    await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name: updatedUser.name,
+        sex: updatedUser.sex,
+        password: encryptPassword(updatedUser.password),
+        phoneNumber: updatedUser.phoneNumber,
+        UserDescription: updatedUser.UserDescription,
+        email: updatedUser.email,
+      },
+    });
+    console.log(await prisma.user.findMany());
+  })
+  .post("/v1/updateArtist", validateToken, async (req, res) => {
+    await prisma.artist.update({
+      name: req.body.name,
+      weiboLink: req.body.weiboLink,
+      qq: req.body.qq,
+      isCV: req.body.isCV,
+      isStaff: req.body.isStaff,
+      voiceType: req.body.voiceType,
+      soundPressure: req.body.soundPressure,
+      demoLink: req.body.demoLink,
+      artistDescription: req.body.artistDescription,
+    });
+  })
+  .get("/v1/searchCV", validateToken, async (req, res) => {
+    let cv = await prisma.artist.findMany({
       where: {
         name: req.query.name,
         isCV: true,
       },
-    })
-  );
-});
-
-//搜索Staff API，目前只能GET用户名完全一致的用户
-app.get("/v1/searchStaff", async (req, res) => {
-  res.send(
-    await prisma.artist.findMany({
+    });
+    res.send(generateToken(cv));
+  })
+  .get("/v1/searchStaff", validateToken, async (req, res) => {
+    let staffs = await prisma.artist.findMany({
       where: {
         name: req.query.name,
         isStaff: true,
       },
-    })
-  );
-});
-
-//删除用户信息，会写入文件
-app.delete("/v1/deleteUser", async (req, res) => {
-  await prisma.user.delete({
-    where: {
-      id: req.query.id,
-    },
+    });
+    res.send(generateToken(staffs));
+  })
+  .delete("/v1/deleteUser", validateToken, async (req, res) => {
+    await prisma.user.delete({
+      where: {
+        id: req.query.id,
+      },
+    });
+  })
+  .delete("/v1/deleteArtist", validateToken, async (req, res) => {
+    await prisma.artist.delete({
+      where: {
+        id: req.query.id,
+      },
+    });
+  })
+  .post(
+    "/v1/uploadAvatar",
+    validateToken,
+    uploads.single("avatar"),
+    (req, res, next) => {
+      let id = req.query.id;
+      let originalName = req.file.originalname;
+      let fileType = originalName.split(".")[1];
+      rename(
+        `./public/${req.file.filename}`,
+        `./assets/images/avatars/${id}.${fileType}`
+      );
+    }
+  )
+  .post(
+    "/v1/uploadDemo",
+    validateToken,
+    uploads.single("demo"),
+    (req, res, next) => {
+      let id = req.query.id;
+      let originalName = req.file.originalname;
+      let fileType = originalName.split(".")[1];
+      rename(
+        `./public/${req.file.filename}`,
+        `./assets/audio/${id}.${fileType}`
+      );
+    }
+  )
+  .get("/v1/getDemo", validateToken, (req, res) => {
+    let id = req.query.id;
+    readFile(`assets/audio/${id}.mp3`, (err, data) => {
+      res.send(data);
+    });
   });
-});
-
-//删除CV/Staff信息，会写入文件
-app.delete("/v1/deleteArtist", async (req, res) => {
-  await prisma.artist.delete({
-    where: {
-      id: req.query.id,
-    },
-  });
-});
-
-//写入用户头像
-app.post("/v1/uploadAvatar", uploads.single("avatar"), (req, res, next) => {
-  let id = req.query.id;
-  let originalName = req.file.originalname;
-  let fileType = originalName.split(".")[1];
-  rename(
-    `./public/${req.file.filename}`,
-    `./assets/images/avatars/${id}.${fileType}`
-  );
-});
-
-//写入用户音频
-app.post("/v1/uploadDemo", uploads.single("demo"), (req, res, next) => {
-  let id = req.query.id;
-  let originalName = req.file.originalname;
-  let fileType = originalName.split(".")[1];
-  rename(`./public/${req.file.filename}`, `./assets/audio/${id}.${fileType}`);
-});
-
-app.get("/v1/getDemo", (req, res) => {
-  let id = req.query.id;
-  readFile(`assets/audio/${id}.mp3`, (err, data) => {
-    res.send(data);
-  });
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on localhost:${process.env.PORT || 3000}`);
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,8 +1,20 @@
-import * as jose from 'jose'
+import jwt from 'jsonwebtoken'
 
-export function generateToken(info){
-    console.log(jose.SignJWT())
+export async function generateToken(user) {
+  return await jwt.sign({user}, "4bdcafe4-5d9b-47b3-97c4-a1817aab5167", {
+    expiresIn: 60 * 60 * 24,
+  });
 }
-export function validateToken(token){
-
+export async function validateToken(req, res, next) {
+  let token = req.headers.authorization;
+  token = token ? token.split("Bearer ")[1] : null;
+  if (!token) {
+    res.status(402).json({ error: "请传入token" });
+  }
+  try {
+    await jwt.verify(token, "4bdcafe4-5d9b-47b3-97c4-a1817aab5167");
+    next();
+  } catch (err) {
+    res.status(402).json({ error: "无有效token值" });
+  }
 }
